bbebdbfe10f6c7b547ebb9a11ee38b57b95cff38
mdawezo <82418711+mdawezo@users.noreply.github.com>
2021-05-17 07:53:56 +0530

    Initial commit

--- index.js
+++ index.js
@@ -136,7 +136,7 @@ slash.on("command", async (command) => {
 
 client.on("ready", () => {
     console.log("This bot is online!");
-    client.user.setActivity('YouTube' , { type: 'WATCHING'}).catch(console.error)
+    client.user.setActivity('YouTube' , { type: 'STREAMING'}).catch(console.error)
     slash.create({
         guildOnly: false,
         data: {
